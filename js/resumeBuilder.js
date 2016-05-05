ee33f278d8783a83ead6bf4e518a70290f25fc68
Tsertsvadze <georgi.tsertsvadze@d-fine.local>
2016-05-05 11:38:15 +0200

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,25 @@
-$("#main").append("Georgi Tsertsvadze");
+/*
+$("#main").append("My name is Georgi Tsertsvadze");
+
+var awesomeThougths=" and I am awesome!";
+var funThougths=awesomeThougths.replace("awesome", "fun");
+console.log(awesomeThougths);
+console.log(funThougths);
 $("#main").append(funThougths);
+
+*/
+
+var formattedName=HTMLheaderName.replace("%data%", "Georgi Tsertsvadze");
+
+var role = "Data Scientist";
+var formattedRole=HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+
+
+/*
+Exception: TypeError: $(...).append is not a function
+@Scratchpad/3:1:1
+*/
