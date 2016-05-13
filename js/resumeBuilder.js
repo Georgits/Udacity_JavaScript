7b9c0f6bb30719d14299d1a462d2f4219491b725
Georgits <georgits@yahoo.de>
2016-05-13 19:27:23 +0200

    pull

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,196 @@
+
+var bio = {
+	"welcomeMessage": "Welcome on my page!",
+	"name": "Georgi Tsertsvadze",
+	"contacts": {
+		"address1": "Louisenstr. 70",
+		"address2": "61348 Bad Homburg",
+		"mobile": "0162-2630085",
+		"email": "georgits@yahoo.de"
+		},
+	"age": 41,
+	"role": "Data Scientist",
+	"skills": [
+		"awesomennes", "programming", "js"
+		],
+	"bioPic": "C:/Users/Georgi/Desktop/CV/IMG_7430_15.jpg"
+}
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+$("#skills").append(formattedSkill);
+	
+var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+$("#skills").append(formattedSkill);
+
+var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+$("#skills").append(formattedSkill);
+
+var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+$("#skills").append(formattedSkill);
+}
+
+
+var education = {
+	"schools": [
+		{
+			"name": "State University of Tbilisi, Georgia",
+			"location": "Tbilisi",
+			"degree": "BA Economics",
+			"dates": "1992-1994",
+			"major": ["Macroeconomics", "International Economics"]
+		},
+		{
+			"name": "Leibniz University of Hanover, Germany",
+			"location": "Hanover",
+			"degree": "Diplom-Oekonom",
+			"dates": "1995-2000",
+			"major": ["Econometrics", "labor Economics"]
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Online Course 1",
+			"school": "Online Course schooo 1",
+			"dates": "Online Course dates 1",
+			"url": "http://www.onlinecourse1.com"
+		},
+		{
+			"title": "Online Course 2",
+			"school": "Online Course schooo 2",
+			"dates": "Online Course dates 2",
+			"url": "http://www.onlinecourse2.com"
+		}	
+	]
+}
+
+
+var work = {
+	"jobs": [
+	{
+		"employer": "FERI EuroRating Services AG",
+		"title": "Senior Analyst",
+		"location": "Bad Homburg",
+		"dates": "2006-2015",
+		"description": "Analyzed economics trends"
+	},
+	{
+		"employer": "d-fine",
+		"title": "Senior Consultant",
+		"location": "Frankfurt",
+		"dates": "2015-2016",
+		"description": "Analyzed Health Care sector"
+	}
+	],
+	"projects": [
+	{
+		"employer": "FERI EuroRating Services AG",
+		"title": "Senior Analyst",
+		"location": "Bad Homburg",
+		"dates": "2006-2015",
+		"description": "Analyzed economics trends"
+	},
+	{
+		"employer": "d-fine",
+		"title": "Senior Consultant",
+		"location": "Frankfurt",
+		"dates": "2015-2016",
+		"description": "Analyzed Health Care sector"
+	}
+	]
+
+}
+
+function displayWork() {
+	for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer+formattedTitle;
+	
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+	
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+	}
+}
+
+displayWork();
+
+
+projects.display = function () {
+	for ( project in projects.project) {
+		$("#projects").append(HTMLprojectStart);
+	}
+		
+}
+/*
+funtion locationizer(work_obj) {
+	var locationArray = [];
+	
+	for (job in work_obj.jobs) {
+		var newLocation = work_obj.jobs[job].location;
+		locationArray.push(newLocation);
+	}
+	return locationArray;
+}
+*/
+/*
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase()+ name[0].slice(1).toLowerCase();
+	
+	return name[0] + " " + name[1];
+}
+$("#main").append(internationalizeButton);
+*/
+
+var projects = {
+	"projects": [
+	{
+		"title": "Project 1",
+		"dates": "XXXX-XXXX",
+		"description": "description of project 1",
+	"images": [
+		"image_11", "image_12"
+		]
+	},
+	{
+		"title": "Project 2",
+		"dates": "YYYY-YYYY",
+		"description": "description of project 2",
+	"images": [
+		"image_21", "image_22"
+		]
+	}
+	]
+}
+
+
+
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+	
+	logClicks(x,y);
+});
+
+
+
+
+
+
+
+
+
 /*
 $("#main").append("My name is Georgi Tsertsvadze");
 
@@ -9,14 +202,30 @@ $("#main").append(funThougths);
 
 */
 
-var formattedName=HTMLheaderName.replace("%data%", "Georgi Tsertsvadze");
+/*var formattedName=HTMLheaderName.replace("%data%", "Georgi Tsertsvadze");
 
 var role = "Data Scientist";
 var formattedRole=HTMLheaderRole.replace("%data%", role);
+*/
+
+/*var work = {};
+work.position = "Senior Consultant";
+work.employer = "d-fine";
+work.years = 1;
+
+
+var education = {};
+education["name"] = "Leibniz University of Hanover";
+education["years"] = "1995-2005";
+education["city"] = "Hanover, Lower Saxony, Germany";
+$("#main").append(education.name);
 
 
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
+$("#main").append(bio.contacts.address1);
+$("#main").append(work["position"]);
+*/
 
 
 /*
